b0942da5465199e0c50e8de4c0ab19a8f6040d00
Saeed Altout <saeedaltoutpro@gmail.com>
2025-07-01 01:01:49 +0300

    fix(HomePage): reorder imports for Hero and StepsToOrder components to improve clarity and maintainability

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,7 +1,7 @@
-import { Hero } from '@/components/sections/hero';
-import { StepsToOrder } from '@/components/sections/steps-to-order';
 import { About } from '@/components/sections/about';
 import { Contact } from '@/components/sections/contact';
+import { Hero } from '@/components/sections/hero';
+import { StepsToOrder } from '@/components/sections/steps-to-order';
 
 const HomePage: React.FC = () => {
   return (
