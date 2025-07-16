e6295a27bca59e7bda6200dd15ff4d552acedcca
Saeed Altout <saeedaltoutpro@gmail.com>
2025-07-16 18:23:40 +0300

    refactor(Navbar, Footer): remove legacy Navbar and Footer components; update imports in HomeLayout to reflect new structure and improve maintainability

--- src/layouts/home-layout.tsx
+++ src/layouts/home-layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 
-import { Navbar } from '@/components/sections/navbar';
-import { Footer } from '@/components/sections/footer';
+import { Navbar } from '@/components/common/navbar';
+import { Footer } from '@/components/common/footer';
 
 export default function HomeLayout() {
   return (
